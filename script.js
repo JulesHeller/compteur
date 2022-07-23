3c67468d182237215f55efa546c984e757443a58
JulesHeller <julesheller0@gmail.com>
2022-07-23 18:45:38 +0200

    initialization

---
+++ script.js
@@ -0,0 +1,28 @@
+let date = new Date(2022, 6, 23, 10, 0, 0);
+
+setInterval(compteur, 1000);
+
+function compteur() {
+    now = new Date();
+    let diff = now - date;
+
+    let heures = (Math.floor(diff / 3600000)).toString();
+    if (heures.length == 1) {
+        heures.toString();
+        heures = "0" + heures;
+    }
+    let minutes = (Math.floor(diff / 60000) - heures * 60).toString();
+    if (minutes.length == 1) {
+        minutes.toString();
+        minutes = "0" + minutes;
+    }
+    let secondes = (Math.floor(diff / 1000) - Math.floor(diff / 60000) * 60).toString();
+    if (secondes.length == 1) {
+        secondes.toString();
+        secondes = "0" + secondes;
+    }
+
+    document.querySelector(".compteur .heures").innerHTML = heures;
+    document.querySelector(".compteur .minutes").innerHTML = minutes;
+    document.querySelector(".compteur .secondes").innerHTML = secondes;
+}
